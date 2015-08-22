e83a93abb8e7ac3a0781519a6812c9b8bac059d8
greg <elorian.mestec@gmail.com>
2015-08-22 11:41:34 -0700

    Moved things to StartApp framework

--- transitbureaucrat.elm
+++ transitbureaucrat.elm
@@ -1,3 +1,16 @@
-import Html exposing (text)
+import Html exposing (text, Html)
+import StartApp.Simple exposing (start)
+import Signal exposing (Address)
 
-main = text "Transit Bureaucrat"
+type alias Model = { numCars:  Int }
+
+type Action = Click | Tick
+
+update : Action -> Model -> Model
+update action oldModel = oldModel
+
+view : Address Action -> Model -> Html
+view address model = text "Transit Bureaucrat"
+
+main = start
+        { model = { numCars = 0 }, update = update, view = view }

f71d34e8b0002afef4521ae38ff5a84e7b7f0a69
greg <elorian.mestec@gmail.com>
2015-08-22 15:11:03 -0700

    title screen to level screen transition
    very basic so far

--- src/transitbureaucrat.elm
+++ src/transitbureaucrat.elm
@@ -1,20 +1,41 @@
 import Html exposing (Html)
+import Html.Events exposing (onClick)
 import StartApp.Simple exposing (start)
 import Signal exposing (Address)
 import Graphics.Element as G
 import Text as T
+import Color
 
-type alias Model = { numCars:  Int }
+type ScreenState = TitleScreen | LevelScreen Int
 
-type Action = Click | Tick
+type alias Model = { numCars: Int, screen: ScreenState }
+
+initialModel : Model
+initialModel =  { numCars = 0, screen = TitleScreen }
+
+type Action = ClickNewGame 
+            | ClickContinue
+            | Tick
 
 update : Action -> Model -> Model
-update action oldModel = oldModel
+update action oldModel =  case action of
+    ClickNewGame -> { oldModel | screen <- LevelScreen 1 }
+    _ -> oldModel
 
 view : Address Action -> Model -> Html
-view address model = Html.fromElement <|
-    G.flow G.down [title, mainGamePane, clock]
+view address model = case model.screen of
+    TitleScreen -> 
+        Html.div []
+            [ Html.fromElement <| titleView,
+              Html.button [ onClick address ClickNewGame] [Html.text "New Game" ],
+              Html.button [ onClick address ClickContinue] [Html.text "Continue" ]
+            ]
+    _ -> Html.fromElement <| G.flow G.down [title, mainGamePane, clock]
 
+titleBackgroundColor = Color.rgb 94 5 135
+
+titleView : G.Element
+titleView = G.image 800 600 "../game_logo.png"
 
 title : G.Element
 title = G.show "Transit Bureaucrat"
@@ -26,4 +47,5 @@ clock: G.Element
 clock = G.show "clock"
 
 main = start
-        { model = { numCars = 0 }, update = update, view = view }
+        { model = initialModel, update = update, view = view }
+

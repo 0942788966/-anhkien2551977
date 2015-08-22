cac206e21e682698f2e490c651309ce6fff91535
greg <elorian.mestec@gmail.com>
2015-08-22 16:21:16 -0700

    Infrastructure for showing emails
    still a little bit wonky

--- src/transitbureaucrat.elm
+++ src/transitbureaucrat.elm
@@ -1,26 +1,27 @@
 import Html exposing (Html)
 import Html.Events exposing (onClick)
+import Html.Attributes
 import StartApp.Simple exposing (start)
 import Signal exposing (Address)
 import Graphics.Element as G
 import Text as T
 import Color
+import Array
+import EmailTexts exposing (emailTexts)
 
-type ScreenState = TitleScreen | ChooseLevelScreen | LevelScreen Int 
+type ScreenState = TitleScreen | ChooseLevelScreen | LevelScreen Int | MessageScreen Int
 
 type alias Model = { numCars: Int, screen: ScreenState }
 
 initialModel : Model
 initialModel =  { numCars = 0, screen = TitleScreen }
 
-type Action = ClickNewGame 
-            | ClickContinue
+type Action = GoToScreen ScreenState
             | Tick
 
 update : Action -> Model -> Model
 update action oldModel =  case action of
-    ClickNewGame -> { oldModel | screen <- LevelScreen 1 }
-    ClickContinue -> { oldModel | screen <- ChooseLevelScreen }
+    GoToScreen newScreen -> { oldModel | screen <- newScreen }
     _ -> oldModel
 
 view : Address Action -> Model -> Html
@@ -32,10 +33,11 @@ view address model = case model.screen of
         in
             Html.div []
             [   Html.fromElement <| titleImage,
-                Html.button [ onClick address ClickNewGame] [Html.text "New Game" ],
-                Html.button [ onClick address ClickContinue] [Html.text "Continue" ]
+                Html.button [ onClick address (GoToScreen <| MessageScreen 0) ] [Html.text "New Game" ],
+                Html.button [ onClick address (GoToScreen ChooseLevelScreen) ] [Html.text "Continue" ]
             ]
     ChooseLevelScreen -> renderChooseLevel model
+    MessageScreen n -> renderMessageScreen n address
     LevelScreen n -> renderLevel n model
 
 renderChooseLevel : Model -> Html
@@ -44,6 +46,34 @@ renderChooseLevel model = Html.div [] [ Html.text "Choose a level" ]
 renderLevel : Int -> Model -> Html
 renderLevel levelNum model = 
     Html.fromElement <| G.flow G.down [title, mainGamePane, clock]
+
+renderMessageScreen : Int -> Address Action -> Html
+renderMessageScreen n address = case Array.get n emailTexts of
+    Just emailText -> emailTemplate emailText address
+    Nothing -> Html.text "Error - no message for this message id"
+
+emailTemplate: String -> Address Action -> Html
+emailTemplate msg address =
+    Html.div [Html.Attributes.style
+                [("backgroundColor", "rgb(94,5,135"),
+                 ("width", "800px"),
+                 ("height", "600px"),
+                 ("color", "white")
+                ]
+             ]
+        [
+            Html.text "Email",
+            Html.br [] [], Html.br [] [],
+            Html.text "From: tklabbernick@super.com",
+            Html.br [] [],
+            Html.text "To: juliana.lopez@transit.municip.tri-cities.gov",
+            Html.br [] [],
+            Html.hr [] [],
+            Html.text msg,
+            Html.br [] [],
+            Html.br [] [],
+            Html.button [onClick address (GoToScreen <| LevelScreen 0) ] [Html.text "Start" ]
+        ]
 
 title : G.Element
 title = G.show "Transit Bureaucrat"

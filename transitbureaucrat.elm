04525b6b6e4af91450f0781a2ac23092ca5e1e40
greg <elorian.mestec@gmail.com>
2015-08-22 11:55:35 -0700

    Division between title and main pane

--- transitbureaucrat.elm
+++ transitbureaucrat.elm
@@ -1,6 +1,8 @@
-import Html exposing (text, Html)
+import Html exposing (Html)
 import StartApp.Simple exposing (start)
 import Signal exposing (Address)
+import Graphics.Element as G
+import Text as T
 
 type alias Model = { numCars:  Int }
 
@@ -10,7 +12,15 @@ update : Action -> Model -> Model
 update action oldModel = oldModel
 
 view : Address Action -> Model -> Html
-view address model = text "Transit Bureaucrat"
+view address model = Html.fromElement <|
+    G.flow G.down [title, mainGamePane]
+
+
+title : G.Element
+title = G.show "Transit Bureaucrat"
+
+mainGamePane : G.Element
+mainGamePane = G.show "main pane"
 
 main = start
         { model = { numCars = 0 }, update = update, view = view }

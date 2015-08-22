0fbca0c9869588d12caceba99d6ee6cc2587f30f
Alex Nisnevich <alex.nisnevich@gmail.com>
2015-08-22 16:11:13 -0700

    drawing a bus, yay

--- src/Network.elm
+++ src/Network.elm
@@ -1,10 +1,10 @@
 module Network where
 
 import Color
-
 import Graphics.Element exposing (Element)
-
 import Graphics.Collage as GC
+import Signal exposing (foldp)
+import Time exposing (every, second)
 
 import Graph exposing (Graph, Node, Edge)
 
@@ -24,6 +24,11 @@ type alias Agent = {
 
 type AgentKind = Bus
 
+getOrFail : Maybe a -> a
+getOrFail maybe =
+  case maybe of
+    Just something -> something
+
 dist : Float -> Float -> Float
 dist x y = sqrt (x^2 + y^2)
 
@@ -33,29 +38,44 @@ example =
        (0.0,0.0), (1.0,0.0), (0.0,1.0), (1.0,1.0), (0.0,2.0), (1.0,2.0), (2.25,2.0)
       ]
       nodes = List.map2 Node [1..7] points
-      edge from to distance = Edge to from (Road distance [])
+      edge from to distance agents = Edge to from (Road distance agents)
       edges = [
-       edge 2 1 1.0,
-       edge 2 4 1.0,
-       edge 2 7 (dist 1 2),
-       edge 3 1 1.0,
-       edge 3 4 1.0,
-       edge 4 6 1.0,
-       edge 5 3 1.0,
-       edge 6 5 1.0,
-       edge 7 6 1.0
+       edge 2 1 1.0 [],
+       edge 2 4 1.0 [],
+       edge 2 7 (dist 1 2) [{kind = Bus, travelled = 1.0}],
+       edge 3 1 1.0 [],
+       edge 3 4 1.0 [],
+       edge 4 6 1.0 [],
+       edge 5 3 1.0 [],
+       edge 6 5 1.0 [],
+       edge 7 6 1.0 []
       ]
   in
   Graph.fromNodesAndEdges nodes edges
+
+along : Point -> Point -> Float -> Point
+along p1 p2 fraction = {x = (1 - fraction) * p1.x + fraction * p2.x, y = (1 - fraction) * p1.y + fraction * p2.y}
+
+getAgentPositions : Edge Road -> Network -> List Point
+getAgentPositions edge network =
+  let road = edge.label
+      fromPoint = Graph.get edge.from network |> getOrFail |> .node |> .label
+      toPoint = Graph.get edge.to network |> getOrFail |> .node |> .label
+      length = road.length
+      agents = road.agents
+  in
+    List.map (\a -> along fromPoint toPoint (a.travelled / length)) agents
 
 roadStyle : GC.LineStyle
 roadStyle = let def = GC.defaultLine in
             { def | width <- size * 10, cap <- GC.Round }
 
-lineStyle : GC.LineStyle
-lineStyle = let def = GC.defaultLine in
-            { def | width <- size / 2, cap <- GC.Round,
-                             color <- Color.yellow, dashing <- [8 * round size, 4 * round size] }
+medianStyle : GC.LineStyle
+medianStyle = let def = GC.defaultLine in
+            { def | width <- size / 2, 
+                    cap <- GC.Round,
+                    color <- Color.yellow, 
+                    dashing <- [8 * round size, 4 * round size] }
 
 size : Float
 size = 2.5
@@ -69,13 +89,23 @@ render net =
     getPair edge = case (Graph.get edge.from net, Graph.get edge.to net) of
                      (Just x, Just y) -> Just (x.node.label, y.node.label)
                      _                -> Nothing
+
     edgeNodePairs = Graph.edges net |> List.filterMap getPair
     edgeLines = List.map (\ (n1, n2) -> GC.segment (loc n1) (loc n2)) edgeNodePairs
-  in
-    let roads = List.map (GC.traced roadStyle) edgeLines
-        lines = List.map (GC.traced lineStyle) edgeLines
-    in
-    GC.collage 1500 1500 <| roads ++ lines
+    agentPositions = Graph.edges net |> List.concatMap (\e -> getAgentPositions e net)
 
-main : Element
-main = render example
+    roads = List.map (GC.traced roadStyle) edgeLines
+    lines = List.map (GC.traced medianStyle) edgeLines
+    agents = List.map (\pt -> GC.move (loc pt) <| GC.filled Color.red (GC.circle 10)) agentPositions
+  in
+    GC.collage 1500 1500 <| roads ++ lines ++ agents
+
+update : Network -> Network
+update net = net
+
+main : Signal Element
+main = 
+  let initialState = example
+      state = foldp (\tick s -> update s) initialState (every second)
+  in
+    Signal.map render state

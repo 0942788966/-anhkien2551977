dcd06593270290924c3769ccab59adbb938970db
greg <elorian.mestec@gmail.com>
2015-08-22 21:12:40 -0700

    Fully introduced the concept of time
    We have real-time clock ticks now

--- src/Model.elm
+++ src/Model.elm
@@ -1,11 +1,15 @@
 module Model where
 
+import Effects
+import Time exposing (Time)
+
 type ScreenState = TitleScreen | ChooseLevelScreen | LevelScreen Int | MessageScreen Int
 
 type GameTime = GameTime Int
 
 type Action = GoToScreen ScreenState
-            | Tick
+            | Tick Time
+            | ToggleAdvancingTime
 
 type alias Model = { numCars: Int,
                     screen: ScreenState,
@@ -17,6 +21,6 @@ initialModel: Model
 initialModel = {
         numCars = 0,
         screen = TitleScreen,
-        time = GameTime 0,
+        time = GameTime 40,
         timeAdvancing = False
     }

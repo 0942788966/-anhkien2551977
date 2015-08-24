eb4978a7f4d50a2694ea63836d39a656826efb5d
Tikhon Jelvis <tikhon@jelv.is>
2015-08-23 23:10:37 -0700

    Merged changes.

--- src/Model.elm
+++ src/Model.elm
@@ -14,7 +14,11 @@ import Network
 import Levels
 import GameScreens exposing (gameScreens, LevelParams, levelParamsList)
 
-type ScreenState = TitleScreen | ChooseLevelScreen | LevelScreen Int | MessageScreen Int
+type ScreenState = TitleScreen 
+                 | ChooseLevelScreen 
+                 | LevelScreen Int
+                 | MessageScreen Int
+                 | EndLevelScreen Int
 
 type GameTime = GameTime Int
 
@@ -27,51 +31,54 @@ type Action = GoToScreen ScreenState
             | ResetState
             | ToggleAdvancingTime
             | ChangeStopOrder StopDirection
+            | EndLevel
 
 type StopDirection = StopUp | StopDown | MakeActiveStopIndex Int
 
 type alias LevelData = {
-    state: Types.State,
-    networkGenerator: Types.Input -> Types.Network,
-    stops: List BusStop,
-    stopToNodeMapping: Dict String NodeId,
-    activeStopIdx: Maybe Int,
-    changesRemaining: Int
+    state             : Types.State,
+    networkGenerator  : Types.Input -> Types.Network,
+    stops             : List BusStop,
+    stopToNodeMapping : Dict String NodeId,
+    activeStopIdx     : Maybe Int,
+    changesRemaining  : Int,
+    timeLimit         : GameTime
 }
 
 defaultLevelData : LevelData
 defaultLevelData = {
-    state = Types.State (Levels.lvl1 [1, 3, 5]) Dict.empty,
-    networkGenerator = Levels.lvl1,
-    stops = [],
+    state             = Types.State (Levels.lvl1 [1, 3, 5]) Dict.empty,
+    networkGenerator  = Levels.lvl1,
+    stops             = [],
     stopToNodeMapping = Dict.empty,
-    activeStopIdx = Nothing,
-    changesRemaining = 0
+    activeStopIdx     = Nothing,
+    changesRemaining  = 0,
+    timeLimit         = GameTime 10000
     }
 
 
 type BusStop = BusStop String
 
 type alias Model = {
-    screen: ScreenState,
-    levelData: LevelData,
-    time: GameTime,
-    timeAdvancing: Bool,
-    realtimeMs: Float,
-    counter: Int,
-    tickRate : Int -- one game time tick every tickRate ms
+    screen        : ScreenState,
+    levelData     : LevelData,
+    time          : GameTime,
+    timeAdvancing : Bool,
+    realtimeMs    : Float,
+    counter       : Int,
+    tickRate      : Int -- one game time tick every tickRate ms
 }
 
 
 initialModel: Model
 initialModel = {
-    screen = TitleScreen,
-    levelData = defaultLevelData,
-    time = GameTime 0,
+    screen        = TitleScreen,
+    levelData     = defaultLevelData,
+    time          = GameTime 0,
     timeAdvancing = False,
-    realtimeMs = 0,
-    counter = 0,
-    tickRate = 10
+    realtimeMs    = 0,
+    counter       = 0,
+    tickRate      = 10
     }
 
 
